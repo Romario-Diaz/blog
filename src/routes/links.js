5db0f9b973aa98c857a7caa47e420622318e6488
Romario-Diaz <domsunday.22@gmail.com>
2024-02-06 21:55:42 -0500

    feat (views) main: Adding Home view styles

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,4 +4,16 @@ route.get('/', (req, res) => {
     res.render('web/home')
 })
 
+route.get('/about', (req, res) => {
+    res.render('web/about')
+})
+
+route.get('/publishments', (req, res) => {
+    res.render('web/publishments')
+})
+
+route.get('/courses', (req, res) => {
+    res.render('web/courses')
+})
+
 module.exports = route
